3939637479904b26c355b03877d067336b51c819
Kaleab007 <abatenehkaleab@gmail.com>
2022-06-25 01:31:42 -0700

    auth

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,11 +1,14 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const { where } = require('sequelize/types')
 
 const { User } = db
 
 router.post('/', async (req, res) => {
-    console.log('IN HERE')
+    let user = await user.findOne({
+   where: { email :req.body.email}
 })
-
+ console.log(User)
+})
 module.exports = router
