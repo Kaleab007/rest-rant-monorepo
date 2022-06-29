b1d5ed8253dfae0cf5349d07d24d9176a94672ea
Kaleab007 <abatenehkaleab@gmail.com>
2022-06-28 21:13:44 -0700

    current

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -5,16 +5,19 @@ const { where } = require('sequelize/types')
 
 const { User } = db
 
-router.post('/', async (req, res) => {
+router.get('/profile', async (req, res) => {try {
     let user = await user.findOne({
-   where: { email :req.body.email}
+   where: {
+    userId:
+}
+}) 
+res.json(user)
+} catch {
+    res.json(null)
+}
 })
-if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {res.status(404).json ({
-    message: `Could not find a user with the provided username and password`
-})
-}else
 
- res.json({User})
-})
+
+
 
 module.exports = router
